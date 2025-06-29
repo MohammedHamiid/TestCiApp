2b45978b94607d5bc110b26ac3eda78a40bf8da2
MohammedHamiid <59752739+MohammedHamiid@users.noreply.github.com>
2025-06-29 15:34:37 +0300

    first commit

---
+++ TestCiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCiApp
+//
+//  Created by Mohammed Hamid on 2025-06-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
